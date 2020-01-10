8e8ea34f69dc11b6627634271ef9cd6f1f1c3814
Jaehong Kang <j.kang@ridi.com>
2020-01-10 18:20:46 +0900

    Change to revision

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/sinoru/minizip.git", .branch("9f7e70eb")),
+        .package(url: "https://github.com/sinoru/minizip.git", .revision("9f7e70eb")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
